1e11b4721e04364664e4862c0367b195ff6a6eab
pjq <pengjianqing@gmail.com>
2012-03-01 15:52:16 +0800

    Add animitation to TabGroupActivity

--- src/me/pjq/tabsample/TabGroupActivity.java
+++ src/me/pjq/tabsample/TabGroupActivity.java
@@ -11,76 +11,89 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Window;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.TranslateAnimation;
 
 /**
- * The purpose of this Activity is to manage the activities in a tab.
- * Note: Child Activities can handle Key Presses before they are seen here.
+ * The purpose of this Activity is to manage the activities in a tab. Note:
+ * Child Activities can handle Key Presses before they are seen here.
+ * 
  * @author Eric Harlow
  */
 public class TabGroupActivity extends ActivityGroup {
 
-    private ArrayList<String> mIdList;
-    
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);       
-        if (mIdList == null) mIdList = new ArrayList<String>();
-    }
-  
-    /**
-     * This is called when a child activity of this one calls its finish method. 
-     * This implementation calls {@link LocalActivityManager#destroyActivity} on the child activity
-     * and starts the previous activity.
-     * If the last child activity just called finish(),this activity (the parent),
-     * calls finish to finish the entire group.
-     */
-  @Override
-  public void finishFromChild(Activity child) {
-      LocalActivityManager manager = getLocalActivityManager();    
-      int index = mIdList.size()-1;
-      
-      if (index < 1) {
-          finish();
-          return;
-      }
-          
-      manager.destroyActivity(mIdList.get(index), true);
-      destroy(mIdList.get(index));
-      mIdList.remove(index); index--;
-      String lastId = mIdList.get(index);
-      Intent lastIntent = manager.getActivity(lastId).getIntent();
-      Window newWindow = manager.startActivity(lastId, lastIntent);
-      setContentView(newWindow.getDecorView());
-  }
-  
-  
-  public boolean destroy(String id) {
+	private ArrayList<String> mIdList;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (mIdList == null)
+			mIdList = new ArrayList<String>();
+	}
+
+	/**
+	 * This is called when a child activity of this one calls its finish method.
+	 * This implementation calls {@link LocalActivityManager#destroyActivity} on
+	 * the child activity and starts the previous activity. If the last child
+	 * activity just called finish(),this activity (the parent), calls finish to
+	 * finish the entire group.
+	 */
+	@Override
+	public void finishFromChild(Activity child) {
+		LocalActivityManager manager = getLocalActivityManager();
+		int index = mIdList.size() - 1;
+
+		if (index < 1) {
+			finish();
+			return;
+		}
+
+		manager.destroyActivity(mIdList.get(index), true);
+		destroy(mIdList.get(index));
+		mIdList.remove(index);
+		index--;
+		String lastId = mIdList.get(index);
+		Intent lastIntent = manager.getActivity(lastId).getIntent();
+		Window newWindow = manager.startActivity(lastId, lastIntent);
+		//newWindow.getDecorView().setAnimation(inFromRightAnimation());
+		setContentView(newWindow.getDecorView());
+	}
+
+	public boolean destroy(String id) {
 		final LocalActivityManager activityManager = getLocalActivityManager();
-		if(activityManager != null){
+		if (activityManager != null) {
 			activityManager.destroyActivity(id, false);
 			// http://code.google.com/p/android/issues/detail?id=12359
 			// http://www.netmite.com/android/mydroid/frameworks/base/core/java/android/app/LocalActivityManager.java
 			try {
-				final Field mActivitiesField = LocalActivityManager.class.getDeclaredField("mActivities");
-				if(mActivitiesField != null){
+				final Field mActivitiesField = LocalActivityManager.class
+						.getDeclaredField("mActivities");
+				if (mActivitiesField != null) {
 					mActivitiesField.setAccessible(true);
 					@SuppressWarnings("unchecked")
-					final Map<String, Object> mActivities = (Map<String, Object>)mActivitiesField.get(activityManager);
-					if(mActivities != null){
+					final Map<String, Object> mActivities = (Map<String, Object>) mActivitiesField
+							.get(activityManager);
+					if (mActivities != null) {
 						mActivities.remove(id);
 					}
-					final Field mActivityArrayField = LocalActivityManager.class.getDeclaredField("mActivityArray");
-					if(mActivityArrayField != null){
+					final Field mActivityArrayField = LocalActivityManager.class
+							.getDeclaredField("mActivityArray");
+					if (mActivityArrayField != null) {
 						mActivityArrayField.setAccessible(true);
 						@SuppressWarnings("unchecked")
-						final ArrayList<Object> mActivityArray = (ArrayList<Object>)mActivityArrayField.get(activityManager);
-						if(mActivityArray != null){
-							for(Object record : mActivityArray){
-								final Field idField = record.getClass().getDeclaredField("id");
-								if(idField != null){
+						final ArrayList<Object> mActivityArray = (ArrayList<Object>) mActivityArrayField
+								.get(activityManager);
+						if (mActivityArray != null) {
+							for (Object record : mActivityArray) {
+								final Field idField = record.getClass()
+										.getDeclaredField("id");
+								if (idField != null) {
 									idField.setAccessible(true);
-									final String _id = (String)idField.get(record);
-									if(id.equals(_id)){
+									final String _id = (String) idField
+											.get(record);
+									if (id.equals(_id)) {
 										mActivityArray.remove(record);
 										break;
 									}
@@ -96,61 +109,90 @@ public class TabGroupActivity extends ActivityGroup {
 		}
 		return false;
 	}
-  
-  
-  /**
-   * Starts an Activity as a child Activity to this.
-   * @param Id Unique identifier of the activity to be started.
-   * @param intent The Intent describing the activity to be started.
-   * @throws android.content.ActivityNotFoundException.
-   */
-  public void startChildActivity(String Id, Intent intent) {     
-//      Window window = getLocalActivityManager().startActivity(Id,intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-      Window window = getLocalActivityManager().startActivity(Id,intent);
-      if (window != null) {
-          mIdList.add(Id);
-          setContentView(window.getDecorView()); 
-      }    
-  }
-  
-  /**
-   * The primary purpose is to prevent systems before android.os.Build.VERSION_CODES.ECLAIR
-   * from calling their default KeyEvent.KEYCODE_BACK during onKeyDown.
-   */
-  @Override
-  public boolean onKeyDown(int keyCode, KeyEvent event) {
-      if (keyCode == KeyEvent.KEYCODE_BACK) {
-          //preventing default implementation previous to android.os.Build.VERSION_CODES.ECLAIR
-    	  onBackPressed();
-          return true;
-      }
-      return super.onKeyDown(keyCode, event);
-  }
 
-  /**
-   * Overrides the default implementation for KeyEvent.KEYCODE_BACK 
-   * so that all systems call onBackPressed().
-   */
-  @Override
-  public boolean onKeyUp(int keyCode, KeyEvent event) {
-      if (keyCode == KeyEvent.KEYCODE_BACK) {
-          //onBackPressed();
-          //return true;
-      }
-      return super.onKeyUp(keyCode, event);
-  }
-  
-  /**
-   * If a Child Activity handles KeyEvent.KEYCODE_BACK.
-   * Simply override and add this method.
-   */
-  @Override
-  public void  onBackPressed  () {
-      int length = mIdList.size();
-      if ( length > 1) {
-          Activity current = getLocalActivityManager().getActivity(mIdList.get(length-1));
-          current.finish();
-      }  
-  }
+	/**
+	 * Starts an Activity as a child Activity to this.
+	 * 
+	 * @param Id
+	 *            Unique identifier of the activity to be started.
+	 * @param intent
+	 *            The Intent describing the activity to be started.
+	 * @throws android.content.ActivityNotFoundException.
+	 */
+	public void startChildActivity(String Id, Intent intent) {
+		// Window window =
+		// getLocalActivityManager().startActivity(Id,intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+		Window window = getLocalActivityManager().startActivity(Id, intent);
+		if (window != null) {
+			mIdList.add(Id);
+			
+			//window.getDecorView().setAnimation(inFromRightAnimation());
+			setContentView(window.getDecorView());
+		}
+	}
+
+	public Animation inFromRightAnimation() {
+		Animation inFromRight = new TranslateAnimation(
+				Animation.RELATIVE_TO_PARENT, +1.0f,
+				Animation.RELATIVE_TO_PARENT, 0.0f,
+				Animation.RELATIVE_TO_PARENT, 0.0f,
+				Animation.RELATIVE_TO_PARENT, 0.0f);
+		inFromRight.setDuration(500);
+		inFromRight.setInterpolator(new DecelerateInterpolator());
+		return inFromRight;
+	}
+
+	public Animation outFromLeftAnimation() {
+		Animation outFromleft = new TranslateAnimation(
+				Animation.RELATIVE_TO_PARENT, -1.0f,
+				Animation.RELATIVE_TO_PARENT, 0.0f,
+				Animation.RELATIVE_TO_PARENT, 0.0f,
+				Animation.RELATIVE_TO_PARENT, 0.0f);
+		outFromleft.setDuration(500);
+		outFromleft.setInterpolator(new AccelerateInterpolator());
+		return outFromleft;
+	}
+
+	/**
+	 * The primary purpose is to prevent systems before
+	 * android.os.Build.VERSION_CODES.ECLAIR from calling their default
+	 * KeyEvent.KEYCODE_BACK during onKeyDown.
+	 */
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			// preventing default implementation previous to
+			// android.os.Build.VERSION_CODES.ECLAIR
+			onBackPressed();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	/**
+	 * Overrides the default implementation for KeyEvent.KEYCODE_BACK so that
+	 * all systems call onBackPressed().
+	 */
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			// onBackPressed();
+			// return true;
+		}
+		return super.onKeyUp(keyCode, event);
+	}
+
+	/**
+	 * If a Child Activity handles KeyEvent.KEYCODE_BACK. Simply override and
+	 * add this method.
+	 */
+	@Override
+	public void onBackPressed() {
+		int length = mIdList.size();
+		if (length > 1) {
+			Activity current = getLocalActivityManager().getActivity(
+					mIdList.get(length - 1));
+			current.finish();
+		}
+	}
 }
-
