80ef2287009bfd9fc7ac15be29e07288745dba49
pjq <pengjianqing@gmail.com>
2012-03-01 14:27:27 +0800

    Add Tab Sample code

---
+++ src/com/pjq/tabsample/SelectActivity.java
@@ -0,0 +1,17 @@
+package com.pjq.tabsample;
+
+import me.pjq.ActivityGroupDemo.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+/**
+ * @author Administrator
+ *
+ */
+public class SelectActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(LayoutInflater.from(getParent()).inflate(R.layout.tabstest1, null));
+    }
+}
--- src/me/pjq/ActivityGroupDemo/ActivityGroupDemoActivity.java
+++ src/me/pjq/ActivityGroupDemo/ActivityGroupDemoActivity.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import android.app.ActivityGroup;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -39,7 +40,7 @@ public class ActivityGroupDemoActivity extends ActivityGroup implements
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.main_activitygroup);
 
 		initUI();
 		setClickListener();
@@ -168,6 +169,18 @@ public class ActivityGroupDemoActivity extends ActivityGroup implements
 			break;
 		}
 
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		return super.onKeyDown(keyCode, event);
+	}
+	
+	@Override
+	public void onBackPressed() {
+		// TODO Auto-generated method stub
+		super.onBackPressed();
 	}
 
 }
